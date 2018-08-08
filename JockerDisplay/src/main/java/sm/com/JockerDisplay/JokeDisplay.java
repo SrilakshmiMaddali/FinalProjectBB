18c8624830eb071b159b75c8c4969009fa810fc7
Srilakshmi Maddali <srilakshmi.maddali@gmail.com>
2018-08-07 22:09:26 -0500

    stage 1 , 2, 3 changes
    Stage 1: start Android mutil-app project
    Stage2: create Java lib
    add Java lib as dependency in app, build.gradle
    make Java a lib as submodule
    Stage3:create Android library
    
    Final goal to access, java library from app, main module and then
    pass on this to android library module, which displays joke
    send from Java lib.

--- JockerDisplay/src/main/java/sm/com/JockerDisplay/JokeDisplay.java
+++ JockerDisplay/src/main/java/sm/com/JockerDisplay/JokeDisplay.java
@@ -1,29 +1,25 @@
-package sm.com.finalproject;
+package sm.com.JockerDisplay;
 
-import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.app.FragmentManager;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Toast;
 
-import sm.com.JockerDisplay.JokeDisplay;
-import sm.com.jocker.MyJocker;
-
-public class MainActivity extends AppCompatActivity {
+public class JokeDisplay extends AppCompatActivity {
+    public static String JOKE_KEY = "Joke key";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_joke);
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.menu_joke, menu);
         return true;
     }
 
@@ -40,14 +36,5 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
-    }
-
-    public void tellJoke(View view) {
-        Toast.makeText(this, "derp", Toast.LENGTH_SHORT).show();
-        MyJocker jocker = new MyJocker();
-        String joke = jocker.getJoke();
-        Intent intent = new Intent(this, JokeDisplay.class);
-        intent.putExtra("Joke key", joke);
-        startActivity(intent);
     }
 }
---
+++ jocker/src/main/java/sm/com/jocker/MyJocker.java
@@ -0,0 +1,7 @@
+package sm.com.jocker;
+
+public class MyJocker {
+    public String getJoke() {
+        return "This is a joke from a Java Library";
+    }
+}
